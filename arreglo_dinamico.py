e3908c34e691b88e0cff559c44cef7fa6435332c
Eduardo <eduardovaldesgarcia@gmail.com>
2017-04-03 15:38:44 -0500

    Add files via upload

--- arreglo_dinamico.py
+++ arreglo_dinamico.py
@@ -44,5 +44,25 @@ def procesa(seq):
     return costo,max_tamano
 
 #print(procesa([choice(['agrega','elimina']) for i in range(20)]))
-print(procesa(genera_grupos(20)))
+#print(procesa(genera_grupos(20)))
+
+def experimento(num_umbrales,num_seq,num_rep,grupos):
+    data=open("valores_arreglo_dinamico.txt",'w')
+    print("umbral_menor\tumbral_mayor\ttam_seq\tnum_rep\tcosto\tmaximo_tamano",file=data)
+    for umbral_menor,umbral_mayor in [[random(), random()] for i in range(num_umbrales)]:
+        if umbral_mayor<umbral_menor:
+            umbral_mayor,umbral_menor=umbral_menor,umbral_mayor
+#            temp=umbral_menor
+#            umbral_menor=umbral_mayor
+#            umbral_mayor=temp
+        for tamano in [2**j for j in range(3,num_seq)]:
+            for rep in range(num_rep):
+                if grupos:
+                    costo,max_tam=procesa(genera_grupos(tamano))
+                else:
+                    costo,max_tam=procesa([choice(['agrega','elimina']) for i in range(tamano)])
+                print("%.2f\t%.2f\t%d\t%d\t%d\t%d"%(umbral_menor,umbral_mayor,tamano,rep+1,costo,max_tam),file=data)
+                
+experimento(10,10,5,1)
+            
             
